4338f536a3ef6c364615ca1ab46dcd8f6e747df7
Deepanshu Shukla <112677229+deepanshu190@users.noreply.github.com>
2024-11-27 04:36:10 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  :- we are using nodejs not browser
+
+/* Always remember code readability should be high & learn 
+from the tc39.es & mdn for the better understanding of the js
+*/
+
+/* number,bigInt,string,boolean,null(standalone value),undefined(not assign any value),
+symbol(unique)
+*/
+let age=18
+console.log(typeof(null))
+console.log(typeof(age))
