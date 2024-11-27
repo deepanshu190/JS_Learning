e72a7c47670dcd82251cf7a874905aed56a436f5
Deepanshu Shukla <112677229+deepanshu190@users.noreply.github.com>
2024-11-27 04:53:45 +0000

    container config done

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,42 @@
+// let score="33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score1=null
+
+// console.log(typeof score1);
+// console.log(typeof (score1));
+
+// let valueInNumber1= Number(score1)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+// let score2=undefined
+// console.log(typeof score2);
+// console.log(typeof (score2));
+
+// let valueInNumber2= Number(score2)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+//  "33"=> 33
+// "33abc"=>NaN
+// true =>1; false=>0
+
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=>true 0=>false
+// ""=>false
+// "Deepanshu"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
